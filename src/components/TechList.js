a81b7044cdf86009e93aacf6daddf37dda7de17e
joseiltonjunior <joseiltonjuniortech@gmail.com>
2020-02-16 17:04:47 -0300

    estruturando o projeto

---
+++ src/components/TechList.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+
+class TechList extends Component {
+  state = {
+    newTech: '',
+    techs: [
+      'Node.js',
+      'ReactJS',
+      'React Native'
+    ]
+  };
+
+  handleInputChange = e => {
+    this.setState({ newTech: e.target.value });
+  }
+
+  render() {
+
+    return (
+      <>
+        <ul>
+          {this.state.techs.map(tech => <li key={tech}>{tech}</li>)}
+        </ul>
+        <input
+          type="text"
+          onChange={this.handleInputChange}
+          value={this.state.newTech}
+        />
+      </>
+    )
+  }
+}
+
+export default TechList;
