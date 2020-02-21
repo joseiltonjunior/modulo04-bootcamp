af626e36d0f027f57814d21b9b624858fc16a0ea
joseiltonjunior <joseiltonjuniortech@gmail.com>
2020-02-20 21:58:24 -0300

    novas implementações

--- src/components/TechList.js
+++ src/components/TechList.js
@@ -4,9 +4,8 @@ class TechList extends Component {
   state = {
     newTech: '',
     techs: [
-      'Node.js',
-      'ReactJS',
-      'React Native'
+      'varrer o quarto',
+      'estudar'
     ]
   };
 
@@ -14,19 +13,49 @@ class TechList extends Component {
     this.setState({ newTech: e.target.value });
   }
 
+  handleSubmit = e => {
+    e.preventDefault();
+
+    this.setState({
+      techs: [...this.state.techs, this.state.newTech],
+      newTech: ''
+    });
+  }
+
+  handleDelete = (tech) => {
+    this.setState({ techs: this.state.techs.filter(t => t !== tech) })
+  }
+
   render() {
 
     return (
-      <>
-        <ul>
-          {this.state.techs.map(tech => <li key={tech}>{tech}</li>)}
-        </ul>
-        <input
-          type="text"
-          onChange={this.handleInputChange}
-          value={this.state.newTech}
-        />
-      </>
+      <div className="container">
+        <h1>Painel de tarefas</h1>
+        <form onSubmit={this.handleSubmit}>
+          <ul>
+            {this.state.techs.map(tech => (
+              <li key={tech}>
+                {tech}
+                <button
+                  onClick={() => this.handleDelete(tech)}
+                  type="button"
+                  className="btn btn-danger"
+                >
+                  X
+                </button>
+              </li>
+            ))}
+          </ul>
+          <input
+            type="text"
+            className="form-control required"
+            placeholder="Nova tarefa"
+            onChange={this.handleInputChange}
+            value={this.state.newTech}
+          />
+          <button className="btn btn-info" type="submit">Cadastrar</button>
+        </form>
+      </div>
     )
   }
 }
